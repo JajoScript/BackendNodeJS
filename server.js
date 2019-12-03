b5dde41ad4f0bbcc7326d5fe7d007b6f2d8064dc
jajoscript <javieralmarzabucarey@gmail.com>
2019-12-03 09:18:08 -0600

    configuración inicial

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 // Importación de modulos.
 const express = require('express');
 
+// Creación de la aplicación.
+var app = express();
+
+// Utilización de las rutas.
+app.use('/', (request, response)=>{
+    response.send('Hola');
+});
+
+// Configuración del puerto.
+app.listen(3000);
+
+// Mensajes por consola.
+console.log('App en http://localhost:3000/');
