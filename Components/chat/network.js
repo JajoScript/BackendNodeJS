ccbd48679213fec4a461eb5608e529631979c9b9
jajoscript <javieralmarzabucarey@gmail.com>
2019-12-04 20:24:58 -0600

    Manejo de rutas para la creacion de chats

---
+++ Components/chat/network.js
@@ -0,0 +1,31 @@
+// Importación de modulos.
+const express = require('express');
+const Response = require('../../Network/response');
+const controller = require("./controller");
+
+// Creación del router.
+const router = express.Router();
+
+// Utilización de las rutas.
+router.post('/', (request, response) => {
+    controller.addChat(request.body.users)
+        .then(data => {
+            Response.success(request, response, data, 201);
+        })
+        .catch( error => {
+            Response.error(request, response, 'Error Interno', 500, error);
+        });
+});
+
+router.get('/', (request, response) => {
+    controller.listChat(request.body.userID)
+        .then(users => {
+            Response.success(request, response, users, 201);
+        })
+        .catch(error => {
+            Response.error(request, response, "error interno", 500, error);
+        });
+});
+
+// Exportación del modulo router.
+module.exports = router;
--- Network/routes.js
+++ Network/routes.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const message = require('../Components/Message/network');
 const user = require('../Components/user/network');
+const chat = require('../Components/chat/network');
 
 // Manejo de rutas.
 const routes = (server) => {
     server.use('/message', message);
     server.use('/user', user);
+    server.use('/chat', chat)
 }
 
 // Exportaci
